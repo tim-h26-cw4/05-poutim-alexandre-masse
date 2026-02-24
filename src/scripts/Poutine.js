294348e17d7829e1c2788d85775a02585e435b1f
Alexandre-Masse <alexandremasse06@hotmail.com>
2026-02-24 14:33:49 -0500

    finit section main et plus

---
+++ src/scripts/Poutine.js
@@ -0,0 +1,21 @@
+export default class Poutine {
+  constructor(element) {
+    this.element = element;
+    this.types = this.element.querySelectorAll('.selection');
+    this.selectedType = '';
+    this.init();
+  }
+  init() {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+    }
+  }
+  selectType() {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.classList.remove('is-active');
+    }
+    this.element.classList.toggle('is-active');
+  }
+}
